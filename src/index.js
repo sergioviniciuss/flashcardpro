fa9410ad2a577ef185af9283b7b6b69694113bd2
sergioviniciuss <sergioviniciuss@gmail.com>
2017-09-17 21:29:45 -0300

    Redux - setting up the store for the whole app

--- src/index.js
+++ src/index.js
@@ -2,11 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
 import {BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import rootReducer from './reducers';
 import App from './components/App';
 import Stack from './components/Stack';
+import { setStack } from './actions';
 
 const store = createStore(rootReducer);
+store.subscribe(() => console.log('store',store.getState()));
+store.dispatch(setStack({id: 0, title: 'example', cards: [] }));
+
 ReactDOM.render(
     <BrowserRouter>
         <Switch>
