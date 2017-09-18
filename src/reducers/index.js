61eb65263e0dbd9e004e47b13988c003af0d9c5c
sergioviniciuss <sergioviniciuss@gmail.com>
2017-09-17 21:20:20 -0300

    Redux - creating the firs reducer

---
+++ src/reducers/index.js
@@ -0,0 +1,13 @@
+import { SET_STACK } from '../actions';
+
+function stack(state = {}, action) {
+    switch (action.type) {
+        case "SET_STACK":
+            return action.stack;
+        default:
+            return state;
+    }
+    return state;
+}
+
+export default stack;
