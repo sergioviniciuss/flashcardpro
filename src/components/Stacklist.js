e08414ec11745133b3368ebbf008bc91dc67e847
sergioviniciuss <sergioviniciuss@gmail.com>
2017-09-19 20:17:15 -0300

    setting the Stack and binding action creators

--- src/components/Stacklist.js
+++ src/components/Stacklist.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import stacks from '../data/stacks.json';
 import { Link } from 'react-router-dom';
+import { setStack } from '../actions';
 
 class Stacklist extends Component {
     render () {
+        console.log ('stacklist props', this.props)
         return (
             <div>
                 {
                     stacks.map(stack => {
                         return (
-                            <Link key={stack.id} to='/stack'>
+                            <Link
+                             key={stack.id} 
+                             to='/stack'
+                             onClick={() => this.props.setStack(stack) }
+                            >
                                 <h4 >{stack.title}</h4>
                             </Link>
                             
@@ -20,5 +27,5 @@ class Stacklist extends Component {
         )
     }
 }
-
-export default Stacklist;
+//first parameter of connect function is null because it doesn't listen to the redux store
+export default connect(null, { setStack })(Stacklist);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux';
-import {BrowserRouter, Switch, Route } from 'react-router-dom';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers';
 import App from './components/App';
@@ -13,11 +13,13 @@ store.subscribe(() => console.log('store',store.getState()));
 store.dispatch(setStack({id: 0, title: 'example', cards: [] }));
 
 ReactDOM.render(
-    <BrowserRouter>
-        <Switch>
-            <Route exact path ='/' component={App} />
-            <Route path ='/stack' component={Stack} />
-        </Switch>
-    </BrowserRouter>, 
+    <Provider store={store}>
+        <BrowserRouter>
+            <Switch>
+                <Route exact path ='/' component={App} />
+                <Route path ='/stack' component={Stack} />
+            </Switch>
+        </BrowserRouter>
+    </Provider>, 
     document.getElementById('root')
 );
