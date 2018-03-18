6ee25c3b76d73d3523325a6679486639aaf5b616
sergioviniciuss <sergioviniciuss@gmail.com>
2018-03-18 21:02:41 +0100

    fixing the way the action was being called

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { SET_STACK } from '../actions';
 
 function stack(state = {}, action) {
     switch (action.type) {
-        case "SET_STACK":
+        case SET_STACK:
             return action.stack;
         default:
             return state;
