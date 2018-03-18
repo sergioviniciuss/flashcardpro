9147f85b5ad5bbccb1347d6d5db78e4c26ba94dd
sergioviniciuss <sergioviniciuss@gmail.com>
2018-03-18 21:17:47 +0100

    Adding bootstrap styles and applying some styles to the components

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,9 +4,9 @@ class Card extends Component {
     render () {
         const { prompt, answer } = this.props.card;
         return (
-            <div>
-                <div><h4>{prompt}</h4></div>
-                <div><h4>{answer}</h4></div>
+            <div className="card">
+                <div className="card-prompt"><h4>{prompt}</h4></div>
+                <div className="card-answer"><h4>{answer}</h4></div>
             </div>
         )
     }
--- src/components/Stack.js
+++ src/components/Stack.js
@@ -8,7 +8,9 @@ class Stack extends Component {
         const {title, cards} = this.props.stack;
         return (
             <div>
-                <Link to='/'>Home</Link>
+                <Link className="link-home" to='/'>
+                    <h4>Home</h4>
+                </Link>
                 <h3>{title}</h3>
                 <br />
                 {
